693e69659e284e3b36683115e07ddaca67faad22
Davidko Barilla <108948879+davidtonnang@users.noreply.github.com>
2023-03-31 19:33:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const list = document.querySelector(".list")
 // Change API to the correct link
 submit.addEventListener("click", () => {
   fetch("https://api.content.tripadvisor.com/api/v1/location/1043070/reviews?language=en&key=87F1E8B704F148F4BC8839809A3CCB28", {
-    method: 'no-cors',
+    method: 'GET',
     header: 'application/json'
   })
     .then((response) => response.json())
