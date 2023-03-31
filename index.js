6fa1835d0a92e9c82c22c977da200f6d51c98dc7
Davidko Barilla <davidtonnang@outlook.com>
2023-03-31 21:12:00 +0200

    commit

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const submit = document.querySelector(".submit-btn")
-
 const list = document.querySelector(".list")
+
+console.log(input)
 
 // Change API to the correct link
 submit.addEventListener("click", () => {
-  fetch("https://api.content.tripadvisor.com/api/v1/location/1043070/reviews?language=nl&key=87F1E8B704F148F4BC8839809A3CCB28", {
+  const input = document.querySelector('.input-text').value
+  fetch(`https://api.content.tripadvisor.com/api/v1/location/${input}/reviews?language=nl&key=87F1E8B704F148F4BC8839809A3CCB28`, {
     method: 'GET',
     header: 'application/json'
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      
-        const li = document.createElement("li")
-        // Change name to the correct object
-        li.textContent += review.title
-        list.appendChild(li)
-      
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    data.forEach((review) => {
+      const li = document.createElement("li");
+      li.textContent = review.title;
+      list.appendChild(li);
+    });
     })
     .catch((error) => console.error(error))
 })
